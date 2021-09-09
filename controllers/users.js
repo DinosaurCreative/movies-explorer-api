ee31d669ed38981dc1c026369d1feab172c15996
German <gimalaevgerman@yandex.ru>
2021-09-10 02:10:58 +0300

    feat: create user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const BadRequestError = require('../errors/BadRequestError');
 const DefaultError = require('../errors/DefaultError');
 const NotFoundError = require('../errors/NotFoundError');
+const ConflictError = require('../errors/ConflictError');
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user)
@@ -36,4 +38,32 @@ module.exports.updateProfile = (req, res, next) => {
       }
       return next(new DefaultError(err.message));
     });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, email, password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        name, email, password: hash,
+      })
+        .then((user) => res.send(user))
+        .catch((err) => {
+          if (err.message.includes('emailError')) {
+            return next(new BadRequestError(/* wrongEmail */));
+          } if (err.message.includes('linkError')) {
+            return next(new BadRequestError(/* badValue */));
+          } if (err.code === 11000 && err.name === 'MongoError') {
+            return next(new ConflictError(/* emailTaken */));
+          } if (err.message.includes('nameError')) {
+            return next(new BadRequestError(/* nameLengthErr */));
+          } if (err.message.includes('aboutError')) {
+            return next(new BadRequestError(/* aboutLengthErr */));
+          }
+          return next(new DefaultError(err.message));
+        });
+    }).catch((err) => next(new DefaultError(err.message)));
 };

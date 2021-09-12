7292ab27842d487867b738dcc749592c8af135d8
German <gimalaevgerman@yandex.ru>
2021-09-12 22:38:34 +0300

    fix: back to cors middleware

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
-app.use(cors());
+app.use(cors);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

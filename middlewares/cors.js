8ea0d501960ef0c3d60cdd398910e5c1cd163eb3
German <gimalaevgerman@yandex.ru>
2021-10-13 14:29:07 +0300

    fix: add return to next func

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -18,7 +18,8 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', allowedMethods);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
 
-  next();
+  return next();
 };
